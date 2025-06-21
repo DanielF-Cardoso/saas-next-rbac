fab362a9cd662d5c74a8267dbd2c2016561c7f5f
DanielF-Cardoso <danielfca122@gmail.com>
2025-06-21 11:18:28 -0300

    feat: adiciona tipagem nas subjects

--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -2,18 +2,19 @@ import type { AbilityBuilder } from '@casl/ability'
 
 import type { AppAbility } from '.'
 import type { User } from './models/user'
+import type { Role } from './roles'
 
-type Roles = 'ADMIN' | 'MEMBER'
 type PermissionsByRole = (
   user: User,
   builder: AbilityBuilder<AppAbility>,
 ) => void
 
-export const permissions: Record<Roles, PermissionsByRole> = {
+export const permissions: Record<Role, PermissionsByRole> = {
   ADMIN(_, { can }) {
     can('manage', 'all') // Admins can manage everything
   },
   MEMBER(_, { can }) {
-    can('invite', 'User') // Members can invite Users
+    can('create', 'Project') // Members can create Projects
   },
+  BILLING() {},
 }
